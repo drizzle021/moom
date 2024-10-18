136711f93a4af47594c4250b69b9027d920f7d4a
drizzle021 <gabrielabraham004@gmail.com>
2024-10-18 23:23:17 +0200

    user profile dialog

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -1,42 +1,92 @@
 <template>
-     <q-dialog v-model="userProfileState" class="full-width" @escape-key="closeDialog" no-backdrop-dismiss no-shake>
-      <q-card>
-        <q-card-section>
-          <div class="text-h6">{{ selectedUser.name }}</div>
-        </q-card-section>
-        <q-card-section>
-          <div class="text-h6">{{ selectedUser.state }}</div>
-        </q-card-section>
-        <q-card-actions>
-          <q-btn flat label="Close" @click="closeDialog"/>
-        </q-card-actions>
-      </q-card>
-    </q-dialog>
+  <q-dialog v-model="userProfileState" class="full-width" @escape-key="closeDialog" no-backdrop-dismiss no-shake>
+   <q-card style="min-width: 400px; max-width: 400px">
+     <div class="bg-primary full-width banner" > </div>
+     <q-item>
+       <q-item-section avatar>
+           <div class="image-wrapper">
+             <img class="profile-image" src="src/assets/kotori.jpg" draggable="false">
+             <q-badge class="profile-badge" color="green-7">
+                     Online
+             </q-badge>
+           </div>
+       </q-item-section>
+       <q-item-section style="word-break: break-word;">
+         <q-item-label lines="5" style="white-space: pre-wrap;">hehe :3</q-item-label>
+       </q-item-section>
+
+     </q-item>
+
+     <q-card-section>
+       <div class="text-h6">XxLajosxX</div>
+       <div>Lakatos Lajos | lajos@gmail.com</div>
+     </q-card-section>
+     <q-card-section>
+       
+     </q-card-section>
+     
+     <q-card-actions>
+       <q-btn flat label="Close" @click="closeDialog" color="primary"/>
+     </q-card-actions>
+   </q-card>
+
+ </q-dialog>
 </template>
 
 <script>
- import { defineComponent } from 'vue'
+import { defineComponent } from 'vue'
 
 export default defineComponent({
-    name: 'UserProfile',
-    components:{},
+ name: 'UserProfile',
+ components:{},
 
-    computed: {     // same as method - but cached
-      userProfileState:{
-        get(){
-          return this.$store.state.ui.userProfileState
-        },
-      },
-      selectedUser:{
-        get(){
-          return this.$store.state.ui.userProfileSelected
-        },
-      }
-    },
-    methods:{
-      closeDialog(){
-        this.$store.commit('ui/toggleUserProfile')
-      }
-    }
+ computed: {
+   userProfileState:{
+     get(){
+       return this.$store.state.ui.userProfileState
+     },
+   },
+   selectedUser:{
+     get(){
+       return this.$store.state.ui.userProfileSelected
+     },
+   }
+ },
+ methods:{
+   closeDialog(){
+     this.$store.commit('ui/toggleUserProfile')
+   }
+ }
 })
 </script>
+
+<style scoped lang="scss">
+.banner{
+height:100px
+}
+.image-wrapper {
+position: relative;
+height: auto;
+width:100px;
+margin-right: 40px;
+}
+.profile-image {
+position: absolute;
+height: auto;
+transform: translateY(-65%);
+width:128px;
+
+border-radius: 50%;
+border: 10px solid white;
+
+}
+.profile-badge{
+position: absolute;
+height: auto;
+left:80px;
+font-size: 1.1em;
+border: 3px solid white;
+user-select: none;
+
+}
+</style>
